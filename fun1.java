c022488cffdc732fa10a704f4e82b811a0b144a6
redondo519 <david.redalo@iesjulianmarias.es>
2024-12-13 18:30:11 +0100

    comit

---
+++ fun1.java
@@ -0,0 +1,21 @@
+public class fun1 {
+
+    //ejercicio uno de funciones
+    //mostrar texto n veces (n esta en funcion eco)
+
+    public static void eco(int n){
+
+        for (int i= 0; i<=n; i++){
+            System.out.println("eco...");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+      int n=5;
+      eco(n);
+
+
+    }
+}
