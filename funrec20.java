cad714acb5907c441972c10db6557eb94261c06b
redondo519 <david.redalo@iesjulianmarias.es>
2025-01-13 20:41:51 +0100

    comit

--- funrec20.java
+++ funrec20.java
@@ -3,24 +3,33 @@ import java.util.Arrays;
 public class funrec20 {
 
 
-    public static void primitiva(int correcto[],int num[], int i){
-        for(i=0;i<num.length;i++){
+    public static int primitiva(int correcto[],int num[]){
+       int aciertos = 0;
+        for(int i=0;i<num.length;i++){
             if(num[i]==correcto[i]){
-                i++;
+                //System.out.print(correcto[i]+" ");
+                //System.out.println();
+                aciertos++;
             }
 
+
         }
+        return aciertos;
     }
+
     public static void main(String[] args) {
 
-        int cont = 0;
-        int voletoCorrecto[]= {1,30,2,4,12};
-        int voletoIncorrecto[]= {1,20,2,4,11};
-        primitiva(voletoCorrecto,voletoIncorrecto,cont);
 
-        System.out.println(Arrays.toString(voletoCorrecto));
-        System.out.println(Arrays.toString(voletoIncorrecto));
-        System.out.println(cont);
+        int voletoCorrecto[]= {1,30,2,4,12,6};
+        int voletoIncorrecto[]= {1,20,2,4,11,6};
+        primitiva(voletoCorrecto,voletoIncorrecto);
+
+
+        //System.out.println(Arrays.toString(voletoCorrecto));
+        //System.out.println(Arrays.toString(voletoIncorrecto));
+
+        System.out.println(primitiva(voletoCorrecto,voletoIncorrecto));
+
 
     }
 }
