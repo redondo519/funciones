9e8e341c9f0cce3c6504b69f343368dff4887b81
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2025-01-22 13:20:05 +0100

    comit

---
+++ fun35.java
@@ -0,0 +1,41 @@
+public class fun35 {
+
+
+    //funcion1
+    public static void fun1(int i){
+
+        System.out.println("Soy la función fun1 y me han llamado en la posición  "+i);
+
+    }
+
+    //funcion 2
+    public static void fun2(int i){
+
+        System.out.println("Soy la función fun2 y me han llamado en la posición  "+i);
+
+    }
+
+    //funcion 3
+    public static void fun3(int i){
+
+        System.out.println("Soy la función fun3 y me han llamado en la posición  "+i);
+
+    }
+
+    //funcion 4
+    public static void fun4(int i){
+
+        System.out.println("Soy la función fun4 y me han llamado en la posición  "+i);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        fun1(1);
+        fun2(2);
+        fun3(3);
+        fun4(4);
+
+    }
+}
